37b638dd9c78b09c9720995a0a61d20834d45d59
Paulo Vitor Rosendo <paulo.vitor.rsd@poli.ufrj.br>
2024-01-31 11:19:33 -0300

    added the dto for mapping

---
+++ src/main/java/com/rosendo/transferSystem/dtos/UserDto.java
@@ -0,0 +1,12 @@
+package com.rosendo.transferSystem.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record UserDto(
+  @NotBlank String userName,
+  @NotBlank String userIdentification,
+  @NotBlank String userEmail,
+  @NotBlank String userPassword
+) {
+
+}
